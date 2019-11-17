06ba49a23dca2bbd68d9e0ee65961d6a921bdee7
hojinlee <infohojin@gmail.com>
2019-11-18 02:36:18 +0900

    first

---
+++ src/Helpers/Helper.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the jinyPHP package.
+ *
+ * (c) hojinlee <infohojin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Jiny;
+
+function yaml($string)
+{
+    return \Jiny\Yaml\Yaml::parse($string);
+}
+
